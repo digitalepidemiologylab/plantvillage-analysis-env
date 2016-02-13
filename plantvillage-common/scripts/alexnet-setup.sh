1fde7d66778ba9af10bef365a1e86b01fddde7ed
S.P. Mohanty <spmohanty91@gmail.com>
2016-02-13 22:56:33 +0100

    Correct path to alexnet model

--- plantvillage-common/scripts/alexnet-setup.sh
+++ plantvillage-common/scripts/alexnet-setup.sh
@@ -10,6 +10,6 @@ if [ ! -d /plantvillage/models/alexnet ]; then
   # cp /home/$1/caffe/models/bvlc_alexnet/bvlc_alexnet.caffemodel /plantvillage/models/alexnet/
 
   # Moving BVLC Alexnet Caffe Model to Custom S3 bucket
-  wget http://plantvillage.s3-website-us-east-1.amazonaws.com/models/bvlc_alexnet.caffemodel -O /plantvillage/models/alexnet/bvlc_alexnet.caffemodel
+  wget http://mining-map.s3-website-us-east-1.amazonaws.com/models/bvlc_alexnet.caffemodel -O /plantvillage/models/alexnet/bvlc_alexnet.caffemodel
   chown -R $1:$1 /plantvillage/models/alexnet/
 fi
